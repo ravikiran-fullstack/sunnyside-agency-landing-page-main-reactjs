6d52a9f4b0d40d7421ca162c4647bed62369c893
ravikiran <ravikiransjce.jobs@gmail.com>
2021-06-21 09:53:54 +0530

    made page full responsiveness to mobile version of the page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,11 +3,11 @@ import { projects } from "../data";
 
 const Projects = () => {
   return (
-    <section className="projectsSection">
+    <section className="projectsSection" id="projects">
       {projects.map((project) => {
         return (
           <div key={project.name} className="project">
-            <img className="projectPicture" src={project.picture} alt={project.name || "project"} />
+            {/* <img className="projectPicture" src={project.picture} alt={project.name || "project"} /> */}
           </div>
         );
       })}
