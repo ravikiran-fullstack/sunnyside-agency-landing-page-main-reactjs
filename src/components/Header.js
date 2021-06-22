82b4052be3b0f26e23d7a9cb97fb3b4dc6c7bae0
ravikiran <ravikiransjce.jobs@gmail.com>
2021-06-22 10:42:40 +0530

    Adding responsiveness to page in mobile resolutions

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,27 +2,50 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="headerDesktop">
-      <ul>
-        <li className="homeTab" >  
-          <a href="#home">
-            sunnyside
-          </a>
-        </li>
-        <li>
-          <a href="#about">About</a>
-        </li>
-        <li>
-          <a href="#services">Services</a>
-        </li>
-        <li>
-          <a href="#projects">Projects</a>
-        </li>
-        <li className="contactsTab">
-          <a href="#contacts">Contacts</a>
-        </li>
-      </ul>
-    </header>
+    <>
+      <header className="headerDesktop">
+        <ul>
+          <li className="homeTab">
+            <a href="#home">sunnyside</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#services">Services</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li className="contactsTab">
+            <a href="#contacts">Contacts</a>
+          </li>
+        </ul>
+      </header>
+      <header className="headerMobile">
+        <ul>
+          <li className="homeTabMobile">
+            <a href="#home">sunnyside</a>
+          </li>
+          <li className="dropdownMenu">
+            <ul>
+              <li>
+                About
+              </li>
+              <li>
+                Services
+              </li>
+              <li>
+                Projects
+              </li>
+              <li>
+                Contacts
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </header>
+    </>
   );
 };
 
